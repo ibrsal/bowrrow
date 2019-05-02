c7277937f1ebd1de809ef08d1cc4dd025496d061
ibrsal <ibrsaldk@gmail.com>
2019-05-02 22:01:49 +0200

    fetch customers related to specific client

--- src/client/components/client-customers/Client-customers.js
+++ src/client/components/client-customers/Client-customers.js
@@ -3,17 +3,28 @@ import React from "react";
 class ClientCustomers extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {customers: []};
+      this.state = {
+          customers: [],
+        clientid : null
+        };
   
      
     }
-  
-   
+  //fetch data using API
+    componentDidMount() {
+        fetch(`/api/clientcustomers/${this.props.match.params.id}`)
+          .then(response => response.json())
+          .then(data => {
+            console.log("------>",data);
+            this.setState({ customers: data });
+          });
+      }
   
    
   
     render() {
         console.log("clientcustomers");
+
       return (
         <div className="container"> 
         <h1>Customers</h1> 
